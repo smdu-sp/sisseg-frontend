021a2a1b562d847ae164b1fbc2e20040318e80b1
Bruno <blv.bruno@gmail.com>
2025-02-17 15:52:44 -0300

    Alteração de textos

--- src/app/(rotas-admin)/exonerados/page.tsx
+++ src/app/(rotas-admin)/exonerados/page.tsx
@@ -3,16 +3,39 @@ import Content from "@/components/Content";
 import Table from "@mui/joy/Table";
 import Box from "@mui/joy/Box";
 
+function pegarMesAtual() {
+  const meses = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio", 
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+  const mesAtual = new Date().getMonth();
+  return meses[mesAtual];
+}
+
 export default async function Exonerados() {
   const Usuarios = await exoneradosServices.usuariosExonerados();
   console.log(Usuarios); // Adicione isso para verificar a estrutura dos dados
+
+  const mesAtual = pegarMesAtual();
+
   return (
-    <Content titulo="Usários Exonerados ou Transferidos">
+    <Content titulo={`Usuários exonerados ou transferidos em ${mesAtual}`}>
       <Box>
         <Table borderAxis="both">
           <thead>
             <tr>              
               <th>RF</th>
+              <th>Usuário</th>
               <th>Nome</th>
             </tr>
           </thead>
@@ -22,6 +45,7 @@ export default async function Exonerados() {
               Usuarios.map((usuario) => (
                 <tr key={usuario.id}>                  
                   <td>{usuario.cpRF}</td>
+                  <td>{`d${usuario.cpRF.slice(0,6)}`}</td>
                   <td>{usuario.cpNome}</td>
                 </tr>
               ))}
